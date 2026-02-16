b1d11efad447bb58ee36e8cb5712030996fba48f
sny-der <snydjona@oregonstate.edu>
2026-02-16 13:54:29 -0800

    Added testing functionality to splitter

---
+++ src/splitter_reassembler/split_README.txt
@@ -0,0 +1,2 @@
+Select split_text from this directory to test splitter and reassembler.
+The reassembled file will appear in a folder labeled received_files in this directory.
---
+++ src/splitter_reassembler/splitter_tester.py
@@ -0,0 +1,21 @@
+import splitter
+from tkinter import Tk, filedialog
+from pathlib import Path
+
+Tk().withdraw()
+filepath = filedialog.askopenfilename()
+print("Selected:", filepath)
+
+
+SAVE_DIR = Path("received_files")
+SAVE_DIR.mkdir(exist_ok=True)
+
+safe_name = Path(filepath).name  # strips directory traversal
+
+output_path = SAVE_DIR / safe_name
+
+splitter.reassemble_file(splitter.split_file(filepath), output_path)
+
+
+
+
